568e2ff3620ecf63e7deee707967125ca7b72683
unknown <nik2002360@gmail.com>
2024-02-20 17:58:15 +0500

    Добавил решение групаша 4задание Dz2

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -80,3 +80,26 @@ for (int i = 0; i < (NaturStr.Length);){
 } 
 
 
+// РЕШЕНИЕ ГРУПАША (СКОРЕЕ ВСЕГО ТО, ЧТО ОЖИДАЛОСЬ):
+// Console.Write("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine()); 
+// int i = 1;
+// int variable = number;
+
+// while (variable != 0){
+//     variable = variable / 10; 
+//     i *= 10; 
+// }
+
+// while (i > 1) { 
+//     i = i / 10;
+//     variable = number / i; 
+//     if (i > 1) {
+//         Console.Write(variable + ", ");
+//         number = number % i;
+//     }
+//     else {
+//         Console.Write(variable);
+//     }
+// }
+
