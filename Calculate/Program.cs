af030ce0b5fd2745ed9a5f306634619457e74afc
unknown <nik2002360@gmail.com>
2024-03-02 11:21:40 +0500

    Calculate Loading

---
+++ Calculate/Program.cs
@@ -0,0 +1,27 @@
+﻿// ====================================== Calculate ===========================================
+
+double InputUserFormul (){
+    Console.Clear();
+    Console.WriteLine("========================== Примитивный калькулятор =======================");
+    double ConvertUserForm = 0;
+    for (int i = 0; i == 0;) {
+    Console.Write("Введите формулу \n(используйте операции \"+\", \"-\", \"*\", \"/\", дробные числа вводите через \",\"): ");
+
+    string? UserFormStr = Console.ReadLine();
+
+    bool CheckInt = double.TryParse(UserFormStr, out ConvertUserForm);
+
+    if (CheckInt) { i++; }
+    else { Console.Clear(); Console.WriteLine("============= Вы ввели некорректную формулу, попробуйте еще раз =============\n"); }
+}
+return ConvertUserForm;
+}
+
+
+void PrintResult (double Calculate){
+    Console.WriteLine("Ответ: " + "...:::Loading:::... КАЛЬКУЛЯТОР В РАЗРАБОТКЕ ...:::Loading:::...");
+}
+
+
+double Calculate = InputUserFormul();
+PrintResult(Calculate);
