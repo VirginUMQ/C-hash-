7538d63817d1b30943d99c629e6be7267b3ab8ae
unknown <nik2002360@gmail.com>
2024-03-03 14:31:49 +0500

    calculate до лучших времён

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,21 +1,31 @@
 ﻿// ====================================== Calculate ===========================================
 
+// #заброшено
+
+// - записать в строку, проверить на символы
+// - пройтись циклом по ней до пробела или знаков + - * / 
+// - записать отдельные части в массив, ar[0] = число до пробела/знака, ar[1] = число от пробела/знака до пробела/знака/конца
+// - условия в зависимости от знаков, ar[0] * ar[1] / ar[2] и т.д. => ответ
+
 double InputUserFormul (){
     Console.Clear();
-    Console.WriteLine("========================== Примитивный калькулятор =======================");
+    Console.WriteLine("=========================== Примитивный калькулятор ========================");
     double ConvertUserForm = 0;
     for (int i = 0; i == 0;) {
-    Console.Write("Введите формулу \n(используйте операции \"+\", \"-\", \"*\", \"/\", дробные числа вводите через \",\"): ");
+    Console.Write("\t\t\t\tВведите формулу \n(используйте операции \"+\", \"-\", \"*\", \"/\", дробные числа вводите через \",\"): ");
 
     string? UserFormStr = Console.ReadLine();
 
     bool CheckInt = double.TryParse(UserFormStr, out ConvertUserForm);
 
     if (CheckInt) { i++; }
-    else { Console.Clear(); Console.WriteLine("============= Вы ввели некорректную формулу, попробуйте еще раз =============\n"); }
+    else { Console.Clear(); Console.WriteLine("================ Вы ввели некорректную формулу, попробуйте еще раз =============="); }
 }
 return ConvertUserForm;
 }
+
+
+
 
 
 void PrintResult (double Calculate){
@@ -24,4 +34,5 @@ void PrintResult (double Calculate){
 
 
 double Calculate = InputUserFormul();
+
 PrintResult(Calculate);
